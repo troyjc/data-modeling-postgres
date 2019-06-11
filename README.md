d38b3342e6ee8afad1febf3bf946a29f8659f916
Troy Chevalier <troyjchevalier@gmail.com>
2019-06-10 18:28:32 -0700

    Added ON CONFLICT for inserts to songs table

--- README.md
+++ README.md
@@ -17,12 +17,12 @@ CREATE TYPE level as ENUM('free', 'paid');
 ![ERD Diagram](https://github.com/troyjc/data-modeling-postgres/blob/master/docs/Postgres%20Modeling%20ERD.png)
 
 # Conflicts
-There can be conflicts when inserting into the tables. The <tt>users</tt> and <tt>artists</tt> tables have clauses to handle this *upsert* logic:
+There can be conflicts when inserting into the tables. The <tt>users</tt>, <tt>artists</tt>, and <tt>songs</tt> tables have clauses to handle this *upsert* logic:
 
 ```SQL
 INSERT INTO users (user_id, first_name, last_name, gender, level)
                    VALUES(%s, %s, %s, %s, %s)
-                   ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
+                   ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level;
 ```
 
 ```SQL
@@ -30,5 +30,11 @@ INSERT INTO artists (artist_id, name, location, lattitude, longitude)
                      VALUES(%s, %s, %s, %s, %s)
                      ON CONFLICT (artist_id) DO UPDATE SET location = EXCLUDED.location,
                                                            lattitude = EXCLUDED.lattitude,
-                                                           longitude = EXCLUDED.longitude
+                                                           longitude = EXCLUDED.longitude;
+```
+
+```SQL
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+                   VALUES(%s, %s, %s, %s, %s)
+                   ON CONFLICT DO NOTHING;
 ```
--- sql_queries.py
+++ sql_queries.py
@@ -64,27 +64,28 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time timestamp,
 # INSERT RECORDS
 
 songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-                            VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+                            VALUES(%s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
                         VALUES(%s, %s, %s, %s, %s)
-                        ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
+                        ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
                         VALUES(%s, %s, %s, %s, %s)
+                        ON CONFLICT DO NOTHING;
 """)
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, lattitude, longitude)
                           VALUES(%s, %s, %s, %s, %s) 
                           ON CONFLICT (artist_id) DO UPDATE SET location = EXCLUDED.location,
                                                                 lattitude = EXCLUDED.lattitude,
-                                                                longitude = EXCLUDED.longitude
+                                                                longitude = EXCLUDED.longitude;
 """)
 
 time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-                        VALUES(%s, %s, %s, %s, %s, %s, %s)
+                        VALUES(%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
